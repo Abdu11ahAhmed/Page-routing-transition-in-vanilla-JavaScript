08231b37a6eb03314cce35de34c2a8ba8df23010
Abdu11ahAhmed <sjsuper872@gmail.com>
2025-02-05 07:18:41 +0200

    Done !

--- nextPage.js
+++ nextPage.js
@@ -1,19 +1,4 @@
 const transition = document.querySelector(".transition");
-const nextPage = document.getElementById("nextPage");
-
-let currect = false;
-nextPage.onclick = function (event) {
-    transition.style.opacity = "1";
-    transition.style.transition = ".5s ease";
-    setTimeout(function () {
-        currect = true;
-        window.open("./nextPage.html", "_self");
-    }, 500);
-    if (currect === false) {
-        event.preventDefault();
-    }
-};
-
 const lastPage = document.getElementById("lastPage");
 
 lastPage.onclick = function (event) {
